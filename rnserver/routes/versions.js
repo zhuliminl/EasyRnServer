bb537092d4586dd5fbdf39729da18c6d2c9e125b
zhuliminl <zhuliminl@gmail.com>
2021-08-18 02:22:21 +0800

    update .gitignore

--- rnserver/routes/versions.js
+++ rnserver/routes/versions.js
@@ -1,23 +1,45 @@
 var express = require('express');
 var path = require('path');
+const fs = require('fs')
 const Fse = require('fs-extra')
 const multer = require('multer')
 var router = express.Router();
 
 
-const bundlePath = './bundles'
+const bundleBasePath = './tmp'
+
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
+    const versionCode = new Date().getTime()
+    const bundlePath = `${bundleBasePath}/${versionCode}`
+    const filename = file.fieldname + path.extname(file.originalname)
+    fs.mkdirSync(bundlePath, { recursive: true })
+    req.passData = {
+      bundlePath,
+      versionCode,
+      filename,
+      body: req.body,
+    }
+    // 写入 json 文件
+
     cb(null, bundlePath)
   },
   filename: function (req, file, cb) {
-    cb(null, file.fieldname + '_' + Date.now() + path.extname(file.originalname));
+    const { passData = {} } = req
+    const {
+      bundlePath = '',
+      versionCode = '',
+      filename = '',
+    } = passData
+
+    cb(null, filename);
   }
 })
 // var upload = multer({ storage: storage, limits: { fileSize: 100000000 } }).any()
 var upload = multer({ storage: storage, limits: { fileSize: 100000000000 } }).single('bundle')
 
-router.post('/bundleUpload', upload, function (req, res, next) {
+
+router.post('/bundleUpload', upload, transformToPackage, function (req, res, next) {
   const { body = {} } = req
   const { file = [] } = req
   console.log('saul Body', body)
@@ -25,6 +47,8 @@ router.post('/bundleUpload', upload, function (req, res, next) {
   if (JSON.stringify(file) === '[]') {
     return res.json({ code: 1001, message: '上传文件为空', data: body })
   }
+
+
   res.json({ code: 200, message: '成功上传 RN 包 ', data: body })
 });
 
@@ -45,6 +69,19 @@ router.get('/bundleCheck', async function (req, res, next) {
     }
   })
 });
+
+function transformToPackage(req, res, next) {
+  const { passData = {} } = req
+  const {
+    bundlePath = '',
+    versionCode = '',
+    filename = '',
+  } = passData
+  fs.writeFileSync(`${bundlePath}/desc.json`, JSON.stringify({ ...passData }, null, 2));
+  // 转化称 package 并生成对应的 差分包
+
+  next()
+}
 
 
 
